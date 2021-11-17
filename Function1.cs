4723868d511f81647f54e082bbd6f9deae7ca39b
Johnson Dubula <jdubula@iqbusiness.net>
2021-11-17 18:01:03 +0200

    Add project files.

---
+++ Function1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionApp54
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+        { 
+            string responseMessage ="Hey";
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
